83a3f470cf1eea1cd83a04e779f5ec42d5942fbc
Morgan French-Stagg <morganfrenchstagg@Morgans-MacBook-Pro-2.local>
2018-09-19 17:30:51 +1200

    added basic section based info

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text, View, Button} from 'react-native';
+import {Text, View, Button, AsyncStorage} from 'react-native';
 import { onSignOut } from '../auth';
 import OSM from '../osm';
 
@@ -8,12 +8,23 @@ import SectionList from '../sectionList';
 export default class NavigationPage extends Component {
     constructor(props){
         super(props);
+        this.state = {
+            selectedSection: {}
+        }
+    }
+
+    async componentDidMount(){
+        var sectionData = await AsyncStorage.getItem('selectedSection');
+        console.log(JSON.parse(sectionData))
+        var sectionJSON = JSON.parse(sectionData)
+        this.setState({selectedSection: sectionJSON});
     }
 
     render() {
         return (
             <View>
                 <Text>Home Page!</Text>
+                <Text> {this.state.selectedSection.groupname + " - " + this.state.selectedSection.sectionname} </Text>
                 <Button
                     title = "Get data"
                     onPress = {async () => {
