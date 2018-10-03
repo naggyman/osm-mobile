e389f4369abe4ca9afd2a63063d35956d0b5c8ef
Morgan French-Stagg <morganfrenchstagg@Morgans-MacBook-Pro-2.local>
2018-10-04 11:37:19 +1300

    event page works!

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-import {Text} from 'react-native';
+import {Text, ScrollView, FlatList, AsyncStorage} from 'react-native';
+import {ListItem} from 'react-native-elements';
+import OSM from '../osm';
 
 export default class EventList extends Component {
     static navigationOptions = {
@@ -9,11 +11,40 @@ export default class EventList extends Component {
 
     constructor(props){
         super(props);
+        this.state = {
+            eventList: [],
+            loading: false
+        }
+    }
+
+    async componentDidMount(){
+        this.refreshData();
+    }
+
+    async refreshData() {
+        await this.setState({loading: true});
+
+        var sectionData = await AsyncStorage.getItem('selectedSection');
+        var sectionJSON = JSON.parse(sectionData)
+        console.log(sectionJSON);
+
+        var eventData = await OSM('ext/events/summary/?action=get&sectionid=' + sectionJSON.section.sectionid + '&termid=' + sectionJSON.term.termid, {});
+        var eventDataJson = JSON.parse(eventData._bodyText);
+        console.log(eventDataJson);
+
+        await this.setState({loading: false, eventList: eventDataJson.items})
     }
 
     render() {
         return (
-            <Text>List of stuff!</Text>
+            <ScrollView>
+                <FlatList 
+                    data={this.state.eventList}
+                    refreshing={this.state.loading}
+                    renderItem={({item}) => <ListItem title={item.name} subtitle={item.date} />}
+                    keyExtractor={(item) => item.eventid} 
+                />
+            </ScrollView>
         );
     }
 }
